9aaaf18fbf8527a621b753c84d0945d49e615add
sangwopark19 <sangwopark19@gmail.com>
2023-10-05 23:06:36 +0900

    counter components state props 설정 완료

---
+++ todo-app/src/components/counter/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import CounterButton from './CounterButton';
+import './Counter.css'
+
+export default function Counter() {
+	const [count, setCount] = useState(0)
+
+	function incrementCounterParentFunction(by) {
+		setCount(count + by)
+	}
+	function decrementCounterParentFunction(by) {
+		setCount(count - by)
+	}
+	function resetCounter() {
+		setCount(0)
+	}
+	return (
+		<div>
+			<span className="totalCount">{count}</span>
+			<CounterButton by={1} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
+			<CounterButton by={2} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+			<CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+			<button className='resetButton' onClick={resetCounter}>reset</button>
+		</div>
+	)
+}
