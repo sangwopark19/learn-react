bfca9ad963c7b9c14029e76ce228d713b3637582
sangwopark19 <sangwopark19@gmail.com>
2023-10-07 22:36:34 +0900

    login form 생성 및 ㅊㄴㄴ추가

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,7 +1,39 @@
+import './TodoApp.css'
 export default function TodoApp() {
 	return (
 		<div className="TodoApp">
 			Todo Management Application
+			<LoginComponent></LoginComponent>
+			{/* <WelcomeComponent></WelcomeComponent> */}
 		</div>
 	);
+}
+
+function LoginComponent() {
+	return (
+		<div className="Login">
+			<div className="LoginForm">
+				<div>
+					<label>User Name</label>
+					<input type="text" name="username"/>
+				</div>
+				<div>
+					<label>Password</label>
+					<input type="password" name="password"/>
+				</div>
+				<div>
+					<button type="button" name="login">login</button>
+				</div>
+			</div>
+			
+		</div>
+	)
+}
+
+function WelcomeComponent() {
+	return (
+		<div className="Welcom">
+			Welcome Component
+		</div>
+	)
 }
