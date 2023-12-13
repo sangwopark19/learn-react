0861cf15dfc8869635e20f2b4c3159a767709e3a
Sangwoo Park <sangwopark19@gmail.com>
2023-12-13 16:03:03 +0900

    백엔드 api 생성 및 통신

--- todo-app/src/components/todo/WelcomeComponent.jsx
+++ todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,7 +1,30 @@
 import { Link, useParams } from "react-router-dom";
 
+import { useState } from "react";
+import { retrieveHelloWorldPathVariable } from "./api/HelloWorldApiService";
+
 export default function WelcomeComponent() {
   const { username } = useParams();
+
+  const [message, setMessage] = useState(null);
+
+  function callHelloWorldRestApi() {
+    console.log("call");
+
+    retrieveHelloWorldPathVariable("ranga")
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log("cleanup"));
+  }
+
+  function successfulResponse(response) {
+    console.log(response);
+    setMessage(response.data.message);
+  }
+
+  function errorResponse(error) {
+    console.log(error);
+  }
 
   return (
     <div className="Welcom">
@@ -9,6 +32,12 @@ export default function WelcomeComponent() {
       <div>
         Manage your todos - <Link to="/todos">Go here</Link>
       </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+          Call Hello World
+        </button>
+      </div>
+      <div className="text-info">{message}</div>
     </div>
   );
 }
