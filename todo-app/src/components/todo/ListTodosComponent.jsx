b2bf2d096fad9697d8747cdfe4f51db3ede31929
Sangwoo Park <sangwopark19@gmail.com>
2023-12-13 18:30:35 +0900

    특정 사용자의 게시물들을 불러오는 기능 구현

--- todo-app/src/components/todo/ListTodosComponent.jsx
+++ todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+
 export default function ListTodosComponent() {
   const today = new Date();
 
@@ -7,16 +10,18 @@ export default function ListTodosComponent() {
     today.getDay()
   );
 
-  const todos = [
-    { id: 1, description: "AWS 공부", done: false, targetDate: targetDate },
-    {
-      id: 2,
-      description: "풀스택 개발 공부",
-      done: false,
-      targetDate: targetDate,
-    },
-    { id: 3, description: "DevOps 공부", done: false, targetDate: targetDate },
-  ];
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => refreshTodos(), []);
+
+  function refreshTodos() {
+    retrieveAllTodosForUsername("in28minutes")
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div className="conta">
       <h1>할일 목록</h1>
@@ -36,7 +41,8 @@ export default function ListTodosComponent() {
                 <td>{todo.id}</td>
                 <td>{todo.description}</td>
                 <td>{todo.done.toString()}</td>
-                <td>{todo.targetDate.toDateString()}</td>
+                {/* <td>{todo.targetDate.toDateString()}</td> */}
+                <td>{todo.targetDate.toString()}</td>
               </tr>
             ))}
           </tbody>
