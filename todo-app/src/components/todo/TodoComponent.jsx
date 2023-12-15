ac68f7a14966a804a513bd376da4e3ae3804bb72
Sangwoo Park <sangwopark19@gmail.com>
2023-12-15 21:44:36 +0900

    formik 라이브러리 활용으로 form데이터 활용

--- todo-app/src/components/todo/TodoComponent.jsx
+++ todo-app/src/components/todo/TodoComponent.jsx
@@ -2,12 +2,14 @@ import { useParams } from "react-router-dom";
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
 import { useEffect, useState } from "react";
+import { Field, Form, Formik } from "formik";
 
 function TodoComponent() {
   const { id } = useParams();
   const username = useAuth().username;
 
   const [description, setDescription] = useState("");
+  const [targetDate, setTargetDate] = useState("");
 
   useEffect(() => retrieveTodos(), [id]);
 
@@ -15,14 +17,47 @@ function TodoComponent() {
     retrieveTodoApi(username, id)
       .then((response) => {
         setDescription(response.data.description);
+        setTargetDate(response.data.targetDate);
       })
       .catch((error) => console.log(error));
+  }
+
+  function onSubmit(values) {
+    console.log(values);
   }
 
   return (
     <div className="container">
       <h1>Enter Todo Details</h1>
-      <div>description: {description}</div>
+      <div>
+        <Formik
+          initialValues={{ description, targetDate }}
+          enableReinitialize={true}
+          onSubmit={onSubmit}
+        >
+          {(props) => (
+            <Form>
+              <fieldset className="form-">
+                <label>Description</label>
+                <Field
+                  type="text"
+                  className="form-control"
+                  name="description"
+                />
+              </fieldset>
+              <fieldset className="form-">
+                <label>Target Date</label>
+                <Field type="date" className="form-control" name="targetDate" />
+              </fieldset>
+              <div>
+                <button className="btn btn-success m-5" type="submit">
+                  Save
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
